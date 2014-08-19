d98764b7aebf26be75f5c35cc6ab054e2ca88488
Nick Velloff <nick.velloff@gmail.com>
2014-08-19 18:51:13 -0400

    init process email

---
+++ process/process.php
@@ -0,0 +1,21 @@
+<?php
+
+require('vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+// add records to the log
+$log->addWarning("Received.");
+
+/*
+$json = '{"foo-bar": 12345}';
+
+$obj = json_decode($json);
+print $obj->{'foo-bar'}; // 12345
+*/
+?>
