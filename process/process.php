eb6da62ea7f8c2c920f1b61e1d5d755224d9cc02
Nick Velloff <nick.velloff@gmail.com>
2014-08-19 19:04:25 -0400

    name change

--- process/process.php
+++ process/process.php
@@ -6,7 +6,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 // create a log channel to STDERR
-$log = new Logger('name');
+$log = new Logger('ccall process handler');
 $log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 
 // add records to the log
