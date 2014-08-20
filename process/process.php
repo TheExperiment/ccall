9d701dc94d0d06523aa86cca4e0c345ddc0aa5ae
Nick Velloff <nick.velloff@gmail.com>
2014-08-19 22:33:58 -0400

    testing reply

--- process/process.php
+++ process/process.php
@@ -12,3 +12,17 @@ $log->pushHandler(new StreamHandler('php://stderr', Logger::INFO));
 $inbound = new \Postmark\Inbound(file_get_contents('php://input'));
 
 $log->addInfo( 'Email received with subject: ' . $inbound->Subject() );
+
+// Response
+$transport = Swift_PostmarkTransport::newInstance( getenv('POSTMARK_API_KEY') );
+
+$mailer = Swift_Mailer::newInstance($transport);
+$message = Swift_Message::newInstance();
+
+// Add stuff to your message
+// $message->setFrom('test@example.com');
+$message->setTo( $inbound->FromEmail() );
+$message->setSubject( $inbound->Subject() );
+$message->setBody('My Reply');
+
+$mailer->send($message);
