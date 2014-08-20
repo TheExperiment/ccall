a326027e40cfe02b61ac875118ff06fdfdd85a45
Nick Velloff <nick.velloff@gmail.com>
2014-08-20 12:40:08 -0400

    detect more

--- process/process.php
+++ process/process.php
@@ -12,11 +12,15 @@ $log->pushHandler(new StreamHandler('php://stderr', Logger::INFO));
 
 $inbound = new \Postmark\Inbound(file_get_contents('php://input'));
 
-if ($inbound->To() !== 'pam@ccall.me') {
-	return;
+/* Waiting until To is available
+if ($inbound->To() != 'pam@ccall.me') {
+	exit(1);
 }
+*/
 
-$log->addInfo( 'Email received from: ' . $inbound->From() );
+$log->addInfo( 'Email received from: ' . $inbound->FromEmail() );
+$log->addInfo( 'Body plain content: ' . $inbound->TextBody() );
+$log->addInfo( 'Body html content: ' . $inbound->HtmlBody() );
 
 
 $re = "/(\\+?(?:(?:9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)|\\((?:9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\\))[0-9. -]{4,14})(?:\\b|x\\d+)/"; 
@@ -25,13 +29,15 @@ $subst = '';
  
 $detectedNumbers = preg_replace($re, $subst, $str);
 
+$log->addInfo( 'detectedNumbers: ' . $detectedNumbers );
+
 // $inbound->TextBody();
 // $inbound->HtmlBody();
 
 // Response
-$postmark = new Postmark( getenv('POSTMARK_API_KEY'), "bot@ccall.me" );
+$postmark = new Postmark( getenv('POSTMARK_API_KEY'), "pam@ccall.me" );
 
-$result = $postmark	->to( $inbound->From() )
+$result = $postmark	->to( $inbound->FromEmail() )
 					->subject( $inbound->Subject() )
 					->plain_message($detectedNumbers)
 					->send();
