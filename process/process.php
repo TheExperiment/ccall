0463d22b24580f70a06b7f2a2c4f395edf26b021
Nick Velloff <nick.velloff@gmail.com>
2014-08-20 20:41:33 -0400

    pam work

--- process/process.php
+++ process/process.php
@@ -18,17 +18,20 @@ $inbound = new \Postmark\Inbound(file_get_contents('php://input'));
  }
  */
 
-$log -> addInfo('Email received from: ' . $inbound -> FromEmail());
-$log -> addInfo('Body plain content: ' . $inbound -> TextBody());
-$log -> addInfo('Body html content: ' . $inbound -> HtmlBody());
+// $log -> addInfo('Email received from: ' . $inbound -> FromEmail());
+// $log -> addInfo('Body plain content: ' . $inbound -> TextBody());
+// $log -> addInfo('Body html content: ' . $inbound -> HtmlBody());
 
 $re = "/(\\+?(?:(?:9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)|\\((?:9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\\))[0-9. -]{4,14})(?:\\b|x\\d+)/mi";
 $str = $inbound -> TextBody();
 
-preg_match_all($re, $str, $matches);
+// preg_match_all($re, $str, $matches);
 
+$log -> addInfo('Before content: ' . $str);
+$matches = preg_replace($re, '~', $str);
+$log -> addInfo('After content: ' . $str);
 
-$log -> addInfo( '>>>> : ' . print_r($matches,1) );
+$log -> addInfo( 'matches>>> : ' . print_r($matches,1) );
 
 /*
 $matchesList = '';
